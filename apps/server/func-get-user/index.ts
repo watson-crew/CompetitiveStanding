13bd7c345f5a2ac9245c672461bd2fb0f3b2436b
Josh Paveley <josh.paveley@bjss.com>
2022-11-16 23:52:52 +0000

    Update pipeline

--- apps/server/func-get-user/index.ts
+++ apps/server/func-get-user/index.ts
@@ -1,6 +1,35 @@
 import { AzureFunction, Context, HttpRequest } from '@azure/functions';
-// import { getUserByMemorableId } from '../repository/userRepository';
-import { PrismaClient } from '@prisma/client';
+import { getUserByMemorableId } from '../repository/userRepository';
+
+// Have some utils to encapsulate default headers & setting responses in the context
+const defaultHeaders = {
+  'Access-Control-Allow-Origin': '*',
+  'Access-Control-Allow-Methods': 'GET',
+  'Content-Type': 'application/json',
+};
+
+const return404 = (context: Context) => {
+  context.res = {
+    headers: defaultHeaders,
+    statusCode: 404,
+  };
+};
+
+const return200 = (context: Context, body: any) => {
+  context.res = {
+    headers: defaultHeaders,
+    statusCode: 200,
+    body,
+  };
+};
+
+const return500 = (context: Context, error: Error) => {
+  context.res = {
+    headers: defaultHeaders,
+    statusCode: 500,
+    body: error.message,
+  };
+};
 
 const httpTrigger: AzureFunction = async function (
   context: Context,
@@ -11,42 +40,22 @@ const httpTrigger: AzureFunction = async function (
 
   context.log(`[func-get-user] Got memorableId: ${memorableId}`);
 
-  let responseBody;
-
   try {
-    context.log(`[func-get-user] Creating prisma client`);
-    const prisma = new PrismaClient()
-
     context.log(`[func-get-user] Finding user by id ${memorableId}`);
 
-    //const user = await getUserByMemorableId(memorableId);
-    const user = await prisma.user.findFirst({
-      where: {
-          memorableId
-      }
-    })
-    // const user = {
-    //   id: 1,
-    //   content: "test"
-    // }
+    const user = await getUserByMemorableId(memorableId);
 
-    context.log(`[func-get-user] Found user ${JSON.stringify(user)}`);
-
-    responseBody = user;
+    if (!user) {
+      context.log(`[func-get-user] No matching user with id: ${memorableId}`);
+      return404(context);
+    } else {
+      context.log(`[func-get-user] Found user: ${user}`);
+      return200(context, user);
+    }
   } catch (e) {
-    context.log(`[func-get-user] Error: ${e.message}`)
-    responseBody = e.message;
+    context.log(`[func-get-user] Error: ${e.message}`);
+    return500(context, e);
   }
-
-
-  context.res = {
-    headers: {
-      'Access-Control-Allow-Origin': '*',
-      'Access-Control-Allow-Methods': 'GET',
-      'Content-Type': 'application/json',
-    },
-    body: responseBody,
-  };
 };
 
 export default httpTrigger;
