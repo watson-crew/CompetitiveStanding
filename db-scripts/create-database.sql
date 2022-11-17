1c7ee750c41a65f93f1ee9afa86c5a393f0b7a56
Stephen Church <stephen.church@bjss.com>
2022-11-17 16:26:24 +0000

    Add files to initialize a 'CompetitiveStanding' DB when creating the SQL Server container

---
+++ db-scripts/create-database.sql
@@ -0,0 +1,2 @@
+CREATE DATABASE CompetitiveStanding;
+GO
---
+++ db-scripts/entrypoint.sh
@@ -0,0 +1,3 @@
+# Taken from https://www.softwaredeveloper.blog/initialize-mssql-in-docker-container
+# Run Microsoft SQl Server and initialization script (at the same time)
+/usr/src/app/initialize-db.sh & /opt/mssql/bin/sqlservr
