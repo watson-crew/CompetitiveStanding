c1381db91259204101b36398d946aeed686fa069
Stephen Church <stephen.church@bjss.com>
2022-11-17 16:00:47 +0000

    Add migration for unique memorableId

---
+++ apps/server/prisma/migrations/20221117155915_unique_memorable_id/migration.sql
@@ -0,0 +1,25 @@
+/*
+  Warnings:
+
+  - A unique constraint covering the columns `[memorableId]` on the table `User` will be added. If there are existing duplicate values, this will fail.
+
+*/
+BEGIN TRY
+
+BEGIN TRAN;
+
+-- CreateIndex
+ALTER TABLE [dbo].[User] ADD CONSTRAINT [User_memorableId_key] UNIQUE NONCLUSTERED ([memorableId]);
+
+COMMIT TRAN;
+
+END TRY
+BEGIN CATCH
+
+IF @@TRANCOUNT > 0
+BEGIN
+    ROLLBACK TRAN;
+END;
+THROW
+
+END CATCH
