a0dd81bd0b4d03f740e86911efff3cc148d20408
Josh Paveley <josh.paveley@bjss.com>
2022-11-18 09:37:51 +0000

    Bit of cleanup

--- apps/web/src/pages/index.tsx
+++ apps/web/src/pages/index.tsx
@@ -6,57 +6,13 @@ import { Location } from "schema";
 
 export default function Index() {
 
-  const games = [
-    {
-      id: 0,
-      player: {
-        id: 0,
-        name: "Pierce",
-        score: 0
-      },
-      opponent: {
-        id: 1,
-        name: "Josh",
-        score: 0
-      }
-    },
-    {
-      id: 1,
-      player: {
-        id: 2,
-        name: "Stephen",
-        score: 0
-      },
-      opponent: {
-        id: 3,
-        name: "Fabs",
-        score: 0
-      }
-    }
-  ]
-
-  const addMatch = () => {
-    // function to add match + players (if they dont exist)
-  }
-
-  const [users, addUser] = useReducer((users: User[], newUser: User) => [...users, newUser], [] as User[])
   const [locations, setLocations] = useState<Location[]>([])
-
-  const userIds = ['jjp', 'pjm', 'stc', 'ad2', '4e8']
 
   const client = new AppClient({BASE: getConfig().publicRuntimeConfig.apiBaseUrl }) 
 
   const fetchUser = (userId: string) => client.user.getUserByMemorableId(userId)
 
   useEffect(() => {
-
-      userIds.forEach(async userId => {
-        try {
-          addUser(await client.user.getUserByMemorableId(userId))
-        } catch (err) {
-          console.error(err);
-        }
-      })
 
       const fetchLocations = async () => {
         try {
