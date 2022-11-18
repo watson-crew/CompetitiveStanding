925407c1d9d905f50cc0807358cc859bb7a99484
Stephen Church <stephen.church@bjss.com>
2022-11-18 12:37:51 +0000

    Update get-user to include location and map prisma model to schema model

--- apps/server/repository/userRepository.ts
+++ apps/server/repository/userRepository.ts
@@ -1,13 +1,33 @@
 import { prisma } from '../lib/prisma'
+import { User } from 'schema'
 
 export const getUsers = async () => {
     return await prisma.user.findMany();
 }
 
-export const getUserByMemorableId = async (id: string) => {
-    return await prisma.user.findFirst({
+export const getUserByMemorableId = async (id: string): Promise<User> => {
+    const user = await prisma.user.findFirst({
         where: {
             memorableId: id
-        }
+        },
+        include: {
+          location: true,
+        },
     })
+
+    if (!user) {
+        return null;
+    }
+
+    // Map to User dto type...
+    const userDto: User = {
+        id: user.id.toString(),
+        memorableId: user.memorableId,
+        firstName: user.firstName,
+        lastName: user.lastName,
+        location: user.location.name, // Optional?
+        profilePicture: user.profilePicture // Optional?
+    }
+
+    return userDto;
 }
