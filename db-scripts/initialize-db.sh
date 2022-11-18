11f28484a74f65d892363c195af7ff07e677a020
Josh Paveley <josh.paveley@bjss.com>
2022-11-18 11:15:13 +0000

    Probably broken it

--- db-scripts/initialize-db.sh
+++ db-scripts/initialize-db.sh
@@ -1,6 +1,16 @@
+#!/bin/bash
 # Wait to be sure that SQL Server came up
-sleep 90s
+
+CREATE_DATABASE_SCRIPT_NAME=create-database.sql
+
+echo "[DB-INITIALISATION] Waiting for DB to start up"
+
+sleep 15s
+
+echo "[DB-INITIALISATION] Executing $CREATE_DATABASE_SCRIPT_NAME"
 
 # Run the setup script to create the DB and the schema in the DB
 # Note: make sure that your password matches what is in the Dockerfile
-/opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P Password1234! -d master -i create-database.sql
+/opt/mssql-tools/bin/sqlcmd -S mssql -U SA -P Password1234! -d master -i /opt/mssql_scripts/$CREATE_DATABASE_SCRIPT_NAME
+
+echo "[DB-INITIALISATION] Finished executing $CREATE_DATABASE_SCRIPT_NAME"
