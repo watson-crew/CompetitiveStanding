d5442bcb14fc9ebfd541238df74c6463018aebc0
Stephen Church <stephen.church@bjss.com>
2022-11-21 15:35:28 +0000

    Seed teams

---
+++ packages/database/src/seed/teams.ts
@@ -0,0 +1,42 @@
+import { Team, User, PrismaClient, Prisma } from '@prisma/client';
+import { toId } from './maps';
+
+type SeedDependencies = {
+    users: User[]
+}
+
+const userToTeam = (user: User): Prisma.TeamCreateInput => usersToTeam([user]);
+
+const usersToTeam = (users: User[]): Prisma.TeamCreateInput => {
+    return {
+        players: {
+            connect: users.map(toId)
+        }
+    }
+}
+
+async function seedTeams(
+  prisma: PrismaClient,
+  { users }: SeedDependencies
+): Promise<Record<string, Team>> {
+  const seededTeams: Record<string, Team> = {};
+
+  // Each user in their own team for 1v1 games
+  const teamsData = users.map(userToTeam);
+
+  for (let i = 0; i < teamsData.length; i++) {
+    const insertedTeam = await prisma.team.upsert({
+      where: {
+        id: i + 1,
+      },
+      update: {},
+      create: teamsData[i],
+    });
+
+    seededTeams[insertedTeam.id] = insertedTeam;
+  }
+
+  return seededTeams;
+}
+
+export default seedTeams;
