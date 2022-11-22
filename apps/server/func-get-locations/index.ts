56be1a3fef9f3754fcd58f61c7732db00a57f065
stephenc29 <stephenjwchurch@gmail.com>
2022-11-22 14:23:32 +0000

    Location endpoints (#47)
    Co-authored-by: Stephen Church <stephen.church@bjss.com>

--- apps/server/func-get-locations/index.ts
+++ apps/server/func-get-locations/index.ts
@@ -1,21 +1,21 @@
-import { operations } from 'schema';
-import locations from '../src/repository/deprecated/locationDb';
+import { operations, Location } from 'schema';
+import { getLocations } from '../src/repository/locationRepository';
 import { EmptyParameterAzureFunction } from '../src/types';
+import { set200Response, set500Response } from '../src/utils/contextUtils';
 
-const httpTrigger: EmptyParameterAzureFunction<operations['getAllLocations']> =
-  async function (context): Promise<void> {
+const httpTrigger: EmptyParameterAzureFunction<
+  operations['getAllLocations']
+> = async function (context): Promise<void> {
     context.log(`[func-get-locations] Returning all locations`);
 
-    context.log(`[func-get-locations] Found ${locations.length} locations`);
-
-    context.res = {
-      headers: {
-        'Access-Control-Allow-Origin': '*',
-        'Access-Control-Allow-Methods': 'GET',
-        'Content-Type': 'application/json',
-      },
-      body: locations,
-    };
+    try {
+      const locations: Location[] = await getLocations();
+      context.log(`[func-get-locations] Found ${locations.length} locations`);
+      set200Response(context, locations);
+    } catch (e) {
+      context.log(`[func-get-location] Error: ${e.message}`);
+      set500Response(context, e);
+    }
   };
 
 export default httpTrigger;
