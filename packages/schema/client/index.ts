293992c4dcdb1ae8f5a7d634628e96d6c58129a6
Josh Paveley <josh.paveley@bjss.com>
2022-11-22 12:48:05 +0000

    Add new InitiateMatch request to schema

--- packages/schema/client/index.ts
+++ packages/schema/client/index.ts
@@ -10,8 +10,11 @@ export { OpenAPI } from './core/OpenAPI';
 export type { OpenAPIConfig } from './core/OpenAPI';
 
 export type { GameType } from './models/GameType';
+export type { InitiateMatchResponse } from './models/InitiateMatchResponse';
 export type { Location } from './models/Location';
+export type { TeamHistoricResult } from './models/TeamHistoricResult';
 export type { User } from './models/User';
 
 export { LocationService } from './services/LocationService';
+export { MatchService } from './services/MatchService';
 export { UserService } from './services/UserService';
