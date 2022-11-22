aba891ab63cf8ecec8aa3a2a8f313e344dd50e37
Stephen Church <stephen.church@bjss.com>
2022-11-22 09:44:11 +0000

    Add location to game types

--- packages/database/src/seed/gameTypes.ts
+++ packages/database/src/seed/gameTypes.ts
@@ -1,17 +1,30 @@
-import { GameType, PrismaClient } from '@prisma/client';
+import { GameType, Prisma, PrismaClient, Location } from '@prisma/client';
 
-const gameTypes: Omit<GameType, "id">[] = [
-  {
-    name: 'Pool',
-    maxNumberOfTeams: 2
-  }
-];
+type SeedDependencies = {
+  locations: Record<string, Location>
+}
+
+const generateGameTypes = (locations: Record<string, Location>): Prisma.GameTypeCreateInput[] => {
+  const nottingham = locations['nottingham']
+
+  return [
+    {
+      name: 'Pool',
+      maxNumberOfTeams: 2,
+      locations: {
+        connect: [{ id: nottingham.id }]
+      }
+    }
+  ]
+};
 
 async function seedGameTypes(
   prisma: PrismaClient,
+  { locations }: SeedDependencies
 ): Promise<Record<string, GameType>> {
   const seededGameTypes: Record<string, GameType> = {};
 
+  const gameTypes = generateGameTypes(locations);
   for (let i = 0; i < gameTypes.length; i++) {
     const insertedGameType = await prisma.gameType.upsert({
       where: {
