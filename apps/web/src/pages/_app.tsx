394aa9c5285f1d03bddba2c11855c0d8559d1424
Stephen Church <stephenjwchurch@gmail.com>
2022-11-25 12:23:20 +0000

    Add persisted storage to redux stores

--- apps/web/src/pages/_app.tsx
+++ apps/web/src/pages/_app.tsx
@@ -2,14 +2,18 @@ import { ApiContext, getApiInstance } from '@src/context/ApiContext';
 import { AppProps } from 'next/app';
 import "../../styles/globals.css"
 import { Provider as StoreProvider } from 'react-redux'
-import store from '@src/store'
+
+import { store, persistor } from '@src/store'
+import { PersistGate } from 'redux-persist/integration/react'
 
 function MyApp({ Component, pageProps }: AppProps) {
 
   return (
     <StoreProvider store={store}>
       <ApiContext.Provider value={getApiInstance()}>
-        <Component {...pageProps} />
+        <PersistGate loading={null} persistor={persistor}>
+          <Component {...pageProps} />
+        </PersistGate>
       </ApiContext.Provider>
     </StoreProvider>
   )
--- apps/web/src/store/index.ts
+++ apps/web/src/store/index.ts
@@ -1,6 +1,20 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { rootReducer } from "./reducers";
 
-export default configureStore({
-    reducer: rootReducer
+import storage from 'redux-persist/lib/storage'
+import { persistReducer, persistStore } from "redux-persist";
+import thunk from 'redux-thunk'
+
+const persistConfig = {
+    key: 'root',
+    storage
+}
+
+const persistedRootReducer = persistReducer(persistConfig, rootReducer)
+
+export const store = configureStore({
+    reducer: persistedRootReducer,
+    middleware: [thunk]
 })
+
+export const persistor = persistStore(store)
