b9815b14a8d5f8aac07d3ca7df6177a0b34a7325
Stephen Church <stephenjwchurch@gmail.com>
2022-11-25 11:27:19 +0000

    Set up initial redux files for storing recent players

---
+++ apps/web/src/store/reducers/playerSlice.ts
@@ -0,0 +1,25 @@
+import { createSlice, PayloadAction } from '@reduxjs/toolkit'
+import { User } from 'schema'
+
+export type PlayerState = {
+    recentlyPlayed: User[]
+}
+
+// Initial state can be a "lazy initializer" to load from localStorage (https://redux-toolkit.js.org/api/createslice#initialstate)
+const initialState: PlayerState = {
+    recentlyPlayed: []
+}
+
+export const playerSlice = createSlice({
+    name: 'players',
+    initialState,
+    reducers: {
+        addRecentlyPlayed: (state, action: PayloadAction<User>) => {
+            state.recentlyPlayed.push(action.payload)
+        },
+    }
+})
+
+export const { addRecentlyPlayed } = playerSlice.actions
+
+export default playerSlice.reducer
