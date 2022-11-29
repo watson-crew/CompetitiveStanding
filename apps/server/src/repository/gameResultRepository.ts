500f4186ebbba3d9d494d870d16b4e66e9a0a5fd
Josh Paveley <32667622+JoshPav@users.noreply.github.com>
2022-11-29 10:30:48 +0000

    Implement initiate game API (#78)
    Co-authored-by: Josh Paveley <josh.paveley@bjss.com>

--- apps/server/src/repository/gameResultRepository.ts
+++ apps/server/src/repository/gameResultRepository.ts
@@ -1,13 +1,135 @@
 import { gameResultMapper } from '@src/mappers/gameResultMapper';
 import { GetResultsForLocationResult } from '@src/types';
-import { prismaClient as prisma } from 'database';
-import { GetRecentMatchesData } from 'schema';
+import { prismaClient as prisma, Prisma } from 'database';
+import {
+  GetRecentMatchesData,
+  InitiateMatchResponse,
+  TeamHistoricResult,
+} from 'schema';
+import dayjs from 'dayjs';
 
-export const getResultsForLocation = async (
+function extractPlayerIds(teamId: string): string[] {
+  if (teamId.length % 3 !== 0) throw new Error();
+
+  return teamId.match(/.{3}/g) || [];
+}
+
+function getPlayersConnect(
+  teamId: string,
+): Prisma.Enumerable<Prisma.UserWhereUniqueInput> {
+  const players = extractPlayerIds(teamId);
+  return players.map(memorableId => ({ memorableId }));
+}
+
+function getTeamCreate(
+  cumulativeTeamId: string,
+): Prisma.TeamCreateWithoutGameResultsInput {
+  return {
+    cumulativeTeamId,
+    players: {
+      // All users should already exist so just a `connect` is fine
+      connect: getPlayersConnect(cumulativeTeamId),
+    },
+  };
+}
+
+async function fetchHistoricResults(
+  gameTypeId: number,
+  participatingTeams: string[],
+): Promise<Record<string, TeamHistoricResult>> {
+  const historicResults = await prisma.gameResult.groupBy({
+    by: ['winningTeamId'],
+    _count: {
+      winningTeamId: true,
+    },
+    where: {
+      gameTypeId,
+      teams: {
+        every: {
+          cumulativeTeamId: {
+            in: participatingTeams,
+          },
+        },
+      },
+      winningTeamId: {
+        not: null,
+      },
+    },
+  });
+
+  const results = Object.fromEntries(
+    historicResults.map(
+      historicResult =>
+        [
+          historicResult.winningTeamId,
+          { wins: historicResult._count.winningTeamId },
+        ] as [string, TeamHistoricResult],
+    ),
+  );
+
+  // Since not all teams will have won, some groups may be null.
+  // So we need to iterate over the original list to build up our teams
+  participatingTeams.forEach(participatingTeam => {
+    if (!results[participatingTeam]) {
+      results[participatingTeam] = {
+        wins: 0,
+      };
+    }
+  });
+
+  return results;
+}
+
+async function createNewMatch(
+  gameTypeId: number,
+  locationId: number,
+  participatingTeams: string[],
+): Promise<number> {
+  const teamsInGame: Prisma.Enumerable<Prisma.TeamCreateOrConnectWithoutGameResultsInput> =
+    participatingTeams.map(teamId => {
+      return {
+        create: getTeamCreate(teamId),
+        where: {
+          cumulativeTeamId: teamId,
+        },
+      };
+    });
+
+  const createResult = await prisma.gameResult.create({
+    data: {
+      startTime: dayjs().toDate(),
+      gameTypeId: gameTypeId,
+      locationPlayedId: locationId,
+      teams: {
+        connectOrCreate: teamsInGame,
+      },
+    },
+  });
+
+  return createResult.id;
+}
+
+export async function initiateNewMatch(
+  gameTypeId: number,
+  locationId: number,
+  participatingTeams: string[],
+): Promise<InitiateMatchResponse> {
+  const [historicResults, matchId] = await Promise.all([
+    fetchHistoricResults(gameTypeId, participatingTeams),
+    createNewMatch(gameTypeId, locationId, participatingTeams),
+  ]);
+
+  return {
+    historicResults,
+    matchId,
+  };
+}
+
+export async function getResultsForLocation(
   locationId: number,
   offset = 0,
   total = 10,
-): Promise<GetRecentMatchesData> => {
+): Promise<GetRecentMatchesData> {
   const matches: GetResultsForLocationResult[] =
     await prisma.gameResult.findMany({
       where: {
@@ -39,4 +161,4 @@ export const getResultsForLocation = async (
     });
 
   return gameResultMapper.map(matches);
-};
+}
