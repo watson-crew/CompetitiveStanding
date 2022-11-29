f5d36dc4a10294dee11d2f21c00f726bb2a4a591
Stephen Church <stephenjwchurch@gmail.com>
2022-11-29 16:20:13 +0000

    Add icons for plays/wins

--- packages/ui/molecules/PlaysAndWinsResults/PlaysAndWinsResults.tsx
+++ packages/ui/molecules/PlaysAndWinsResults/PlaysAndWinsResults.tsx
@@ -1,5 +1,7 @@
 import { WithDefaultProps } from "../../types"
 import Text from "../../atoms/Text/Text"
+import { IoMdTrophy } from 'react-icons/io'
+import { IoLogoGameControllerB } from 'react-icons/io'
 
 type PlaysAndWinsResultsProps = WithDefaultProps<{
     gamesPlayed: number,
@@ -14,8 +16,8 @@ export default function PlaysAndWinsResults({ className, gamesPlayed, gamesWon, 
         <div className={className}>
             {fullVersion &&
             <section>
-                <Text type='h2' className='text-sky-500 dark:text-sky-400'>Wins: <span>{gamesWon}</span></Text>
-                <Text type='p' className='text-[#ff3e00] font-bold'>Played: <span>{gamesPlayed}</span></Text>
+                <Text type='h2' className='text-sky-500 dark:text-sky-400'><span><IoMdTrophy/>{gamesWon}</span></Text>
+                <Text type='p' className='text-[#ff3e00] font-bold'><span><IoLogoGameControllerB/>{gamesPlayed}</span></Text>
             </section>
             }
             {!fullVersion &&
