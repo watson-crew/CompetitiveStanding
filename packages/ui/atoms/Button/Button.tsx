530f025ec929e9a90b2b519ae4fa100804b1e909
Josh Paveley <josh.paveley@bjss.com>
2022-11-30 16:32:42 +0000

    Merge conflict

--- packages/ui/atoms/Button/Button.tsx
+++ packages/ui/atoms/Button/Button.tsx
@@ -6,6 +6,7 @@ type ButtonProps = WithDefaultProps<{
   text?: string;
   disabled?: boolean;
   onClick: () => void;
+  type?: 'submit' | 'button';
 }>;
 
 export default function Button({
@@ -14,12 +15,13 @@ export default function Button({
   onClick,
   disabled,
   className = '',
+  type = 'button',
 }: ButtonProps) {
   const hasChildren = children !== undefined;
 
   return (
     <button
-      type="button"
+      type={type}
       disabled={disabled}
       className={twMerge(
         `font-small disabled: relative my-5 w-fit cursor-pointer rounded bg-yellow-600 py-2 px-4 text-center text-lg text-white shadow-md enabled:hover:bg-yellow-500 disabled:cursor-default disabled:opacity-25`,
