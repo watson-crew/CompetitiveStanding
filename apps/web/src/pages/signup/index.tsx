86b8b35c98e10f96af8bd9050bc6b925786e3aa8
Pierce Morris <pierce.morris1998@gmail.com>
2022-11-30 15:13:14 +0000

    clean some user validation

--- apps/web/src/pages/signup/index.tsx
+++ apps/web/src/pages/signup/index.tsx
@@ -1,32 +1,32 @@
 import { CreateUserPayload } from "schema";
 import { ApiContext } from "@src/context/ApiContext";
 import { ChangeEvent, FormEvent, useContext, useEffect, useReducer } from "react";
-import { Button, Card, TextInput } from "ui";
+import { Banner, Button, Card, Text, TextInput } from "ui";
 import { Actions, initialState, signupReducer } from "./state";
 
 export default function Index() {
   const api = useContext(ApiContext)
-  const [inputs, dispatch] = useReducer(signupReducer, initialState);
+  const [signupState, dispatch] = useReducer(signupReducer, initialState);
 
   useEffect(() => {
-    if (inputs.memorableId.length === 3) { 
-      doesMemorableIdExist(inputs.memorableId)
+    if (signupState.memorableId.length === 3) { 
+      doesMemorableIdExist(signupState.memorableId)
     }
-  }, [inputs.memorableId])
+  }, [signupState.memorableId])
 
   const handleOnChange = (action: Actions, event: ChangeEvent<HTMLInputElement>) => {
     dispatch({ action, value: event.target.value })
   }
 
+  const handleError = (message: string) => {
+    if (signupState.errorMessages.find(text => text === message) === undefined)
+      dispatch({ action: Actions.setError, errorMessage: message })
+  }
+
   const createUser = async (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault()
-
-    const { firstName, lastName, memorableId, memorableIdExists } = inputs
-    if (
-      firstName.length === 0 ||
-      lastName.length === 0 ||
-      memorableId.length === 0 ||
-      memorableIdExists) return null
+    let error = false
+    const { firstName, lastName, memorableId, memorableIdExists } = signupState
 
     const user: CreateUserPayload = {
       firstName,
@@ -34,12 +34,27 @@ export default function Index() {
       memorableId,
       homeLocationId: 1
     }
+    
+    if (
+      firstName.length === 0 ||
+      lastName.length === 0 ||
+      memorableId.length === 0
+    ) {
+      error = true
+      handleError('Please fill in all the fields')
+    }
+    
+    if (memorableIdExists) {
+      error = true
+      handleError('Please fill in a unique player ID')
+    }
 
     try {
+      if (error) return 
       await api.user.createUser(user)
       dispatch({ action: Actions.resetState })
     } catch (e) {
-      console.log(`error creating player ${e}`)
+      handleError('Error creating player')
     }
   }
 
@@ -55,11 +70,17 @@ export default function Index() {
   return (
     <div className="flex h-screen flex-col items-center mt-20">
       <h1 className="text-3xl font-bold underline mb-24">Sign up</h1>
+        { signupState.errorMessages.length > 0 ? 
+          <Banner className="w-96 mb-6" type="error">{signupState.errorMessages.map((error, index) => 
+            <Text key={index} type="p">
+              {error}
+            </Text>)}
+          </Banner> : null }
         <Card className="w-96">
           <form onSubmit={createUser}>
-            <TextInput id="firstName" title="First Name" value={inputs.firstName} onChange={(event) => handleOnChange(Actions.firstNameChange, event)} maxLength={24} />
-            <TextInput id="lastName" title="Last Name" value={inputs.lastName} onChange={(event) => handleOnChange(Actions.lastNameChange, event)} maxLength={24} />
-            <TextInput id="memorableId" title="Memorable ID" value={inputs.memorableId} onChange={(event) => handleOnChange(Actions.memorableIdChange, event)} maxLength={3} />
+            <TextInput id="firstName" title="First Name" value={signupState.firstName} onChange={(event) => handleOnChange(Actions.firstNameChange, event)} maxLength={24} />
+            <TextInput id="lastName" title="Last Name" value={signupState.lastName} onChange={(event) => handleOnChange(Actions.lastNameChange, event)} maxLength={24} />
+            <TextInput id="memorableId" title="Memorable ID" value={signupState.memorableId} onChange={(event) => handleOnChange(Actions.memorableIdChange, event)} maxLength={3} />
             <Button type='submit' text="Create player" className="w-full mt-8 mb-2" />
           </form>
         </Card>
--- apps/web/src/pages/signup/state.ts
+++ apps/web/src/pages/signup/state.ts
@@ -3,19 +3,22 @@ export enum Actions {
   lastNameChange = 'lastNameChange',
   memorableIdChange = 'memorableIdChange',
   memorableIdExists = 'memorableIdExists',
-  resetState = 'resetState'
+  resetState = 'resetState',
+  setError = 'setError'
 }
 
 export type SignupAction = {
   action: Actions,
-  value?: any
+  value?: any,
+  errorMessage?: string | undefined
 }
 
 export type SignupState = {
   firstName: string,
   lastName: string,
   memorableId: string,
-  memorableIdExists: boolean
+  memorableIdExists: boolean,
+  errorMessages: Array<string | undefined>
 }
 
 export function signupReducer(state: SignupState, action: SignupAction): SignupState {
@@ -23,22 +26,33 @@ export function signupReducer(state: SignupState, action: SignupAction): SignupS
     case Actions.firstNameChange:
       return {
         ...state,
+        errorMessages: [],
         firstName: action.value
       }
     case Actions.lastNameChange:
       return {
         ...state,
+        errorMessages: [],
         lastName: action.value
       }
     case Actions.memorableIdChange:
       return {
         ...state,
+        errorMessages: [],
         memorableId: action.value
       }
     case Actions.memorableIdExists:
       return {
         ...state,
         memorableIdExists: action.value
+      }
+    case Actions.setError:
+      return {
+        ...state,
+        errorMessages: [
+          ...state.errorMessages,
+          action.errorMessage
+        ]
       }
     case Actions.resetState:
       return initialState
@@ -47,9 +61,10 @@ export function signupReducer(state: SignupState, action: SignupAction): SignupS
   }
 }
 
-export const initialState = {
+export const initialState: SignupState = {
   firstName: '',
   lastName: '',
   memorableId: '',
-  memorableIdExists: false
+  memorableIdExists: false,
+  errorMessages: []
 }
