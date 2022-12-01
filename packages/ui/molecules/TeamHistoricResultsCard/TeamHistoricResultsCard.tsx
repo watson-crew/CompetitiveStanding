6597122177086c2f626d3d74a1fed17f02c5a9a0
stephenc29 <stephenjwchurch@gmail.com>
2022-12-01 15:42:47 +0000

    Small player card (#103)

--- packages/ui/molecules/TeamHistoricResultsCard/TeamHistoricResultsCard.tsx
+++ packages/ui/molecules/TeamHistoricResultsCard/TeamHistoricResultsCard.tsx
@@ -43,7 +43,7 @@ export default function TeamHistoricResultsCard({
 
       <WithScrollbar className="flex-grow items-center justify-center">
         {team.players.map(player => (
-          <PlayerCard player={player} className="h-1/2" />
+          <PlayerCard player={player} variant="s"/>
         ))}
       </WithScrollbar>
 
