b5991b8201248c5d8b0f2c80ab467631101d8bab
Josh Paveley <josh.paveley@bjss.com>
2022-12-09 19:27:40 +0000

    Remove slice

--- apps/web/src/store/reducers/index.ts
+++ apps/web/src/store/reducers/index.ts
@@ -1,10 +1,8 @@
 import { combineReducers } from '@reduxjs/toolkit';
 import playerReducer from './playerSlice';
-import locationReducer from './locationSlice';
 
 export const rootReducer = combineReducers({
   players: playerReducer,
-  locations: locationReducer,
 });
 
 export type RootState = ReturnType<typeof rootReducer>;
