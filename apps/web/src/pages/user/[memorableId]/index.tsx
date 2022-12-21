806b9dfbe5ce6d59937c9e7c36853e9e8aac60cb
Ruairidh Taylor <ruairidh.taylor@bjss.com>
2022-12-21 16:01:07 +0000

    eh idk

--- apps/web/src/pages/user/[memorableId]/index.tsx
+++ apps/web/src/pages/user/[memorableId]/index.tsx
@@ -1,4 +1,4 @@
-import { GameType, User } from 'schema';
+import { GameType, GetStatsByMemorableIdData, User } from 'schema';
 import { ApiContext, getApiInstance } from '@src/context/ApiContext';
 import { PagePropsWithLocation } from '@src/utils/staticPropUtils';
 import {
@@ -69,7 +69,7 @@ export default function Index({ user }: UserPageProps) {
   const [recentMatches, setRecentMatches] = useState<GameResult[]>([]);
   const [loadingRecentMatches, setLoadingRecentMatches] = useState(true);
 
-  // const [playerStats, setPlayerStats] = useState<GetStatsByMemorableIdData>();
+  const [playerStats, setPlayerStats] = useState<GetStatsByMemorableIdData>();
 
   const fetchRecentGames = async (user: User) => {
     const data = await api.player.getRecentMatchesByMemorableId(
@@ -79,8 +79,8 @@ export default function Index({ user }: UserPageProps) {
     setRecentMatches(mapRecentResults(data, gameTypes));
     setLoadingRecentMatches(false);
 
-    // const stats = await api.player.getStatsByMemorableId(user.memorableId);
-    // setPlayerStats(stats);
+    const stats = await api.player.getStatsByMemorableId(user.memorableId);
+    setPlayerStats(stats);
   };
 
   useEffect(() => {
@@ -96,7 +96,7 @@ export default function Index({ user }: UserPageProps) {
         variant="m"
       ></PlayerCard>
       <div className="flex items-center   px-4 py-6 ">
-        {/* <div className="mr-6 flex-1 text-center">
+        <div className="mr-6 flex-1 text-center">
           <h3 className="text-lg font-bold leading-tight">Games Played</h3>
           <div className="text-gray-700">{playerStats?.gamesPlayed}</div>
         </div>
@@ -126,7 +126,7 @@ export default function Index({ user }: UserPageProps) {
         <div className="flex-1 text-center">
           <h3 className="text-lg font-bold leading-tight">Nemesis</h3>
           <div className="text-gray-700">{playerStats?.nemesis?.firstName}</div>
-        </div> */}
+        </div>
       </div>
 
       <RecentMatchesOverview
